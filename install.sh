e4f327e8ab3284a471f084bf5f8d584dfdf78491
immanuwell <pchpr.00@list.ru>
2026-08-15 12:12:59 +0400

    chore: bump version to 0.2.0 since the 0.1.0 tag never produced a working release

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 #
 #   curl -fsSL ewry.net/feint.sh | sh
 #
-# Set FEINT_VERSION to pin a version (e.g. FEINT_VERSION=0.1.0). Set
+# Set FEINT_VERSION to pin a version (e.g. FEINT_VERSION=0.2.0). Set
 # FEINT_INSTALL_DIR to install somewhere other than ~/.local/bin.
 
 set -eu
--- packaging/homebrew/feint.rb
+++ packaging/homebrew/feint.rb
@@ -6,22 +6,22 @@
 class Feint < Formula
   desc "Deterministic, Postgres-native synthetic test data"
   homepage "https://github.com/immanuwell/feint"
-  version "0.1.0"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/immanuwell/feint/releases/download/0.1.0/feint-0.1.0-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/immanuwell/feint/releases/download/0.2.0/feint-0.2.0-aarch64-apple-darwin.tar.gz"
       sha256 "REPLACE_WITH_REAL_SHA256"
     end
     on_intel do
-      url "https://github.com/immanuwell/feint/releases/download/0.1.0/feint-0.1.0-x86_64-apple-darwin.tar.gz"
+      url "https://github.com/immanuwell/feint/releases/download/0.2.0/feint-0.2.0-x86_64-apple-darwin.tar.gz"
       sha256 "REPLACE_WITH_REAL_SHA256"
     end
   end
 
   on_linux do
-    url "https://github.com/immanuwell/feint/releases/download/0.1.0/feint-0.1.0-x86_64-unknown-linux-gnu.tar.gz"
+    url "https://github.com/immanuwell/feint/releases/download/0.2.0/feint-0.2.0-x86_64-unknown-linux-gnu.tar.gz"
     sha256 "REPLACE_WITH_REAL_SHA256"
   end
 
